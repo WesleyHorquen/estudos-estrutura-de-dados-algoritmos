604730b55ec0f40da45c7da8879307320c917687
Drevinicius <anvnc01@gmail.com>
2025-10-08 16:44:08 -0400

    teste

--- Trabalho/SelectionSort/main.c
+++ Trabalho/SelectionSort/main.c
@@ -2,45 +2,49 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TAM 10
+
+// Ele criar um vetor alocando mémoria dinamicamente
 int * criarVetor(int tam){
     if(tam <= 0){
-        printf("> Tamanho invalido!\n");
+        printf("> Tamanho invalido!\n");// Caso tamanho seja 0 ele é inválido
         exit(1);
     }
-    int *vetor = malloc(tam * sizeof(int));
-    if(vetor == NULL){
-        printf("> Erro ao alocar memoria!\n");
-        exit(1);
-    }
-    return vetor;
-}
 
+    int *vetor = malloc(tam * sizeof(int)); // Faz a alocação dinâmica
+
+    if(vetor == NULL){
+        printf("> Erro ao alocar memoria!\n"); // Verifica se foi alocado certo
+        exit(1);
+    }
+    return vetor; // Caso a alocação e o tamanho seja válido ele retorna o ponteiro do meu vetor
+}
+// Função pra preencher o vetor com valores aleatórios de 0 a 100
 int * preencherVetor(int *vetor, int tam){
     for(int i = 0; i < tam; i++){
         vetor[i] = rand() % 101;
     }
     return vetor;
 }
-
 void exibirVetor(int *vetor, int tam){
     for(int i = 0; i < tam; i++){
         printf("| %d ", vetor[i]);
     }
     printf("|\n");
 }
-
-void ordenarVetor(int *vetor, int tam){
+// Função Selection Sort
+void selectionSort(int *vetor, int tam){
     for(int i = 0; i < tam - 1; i++){
-        int aux = i;
+        int aux = i; // Inicia com o auxiliar dizendo que é o menor valor
         for(int j = i + 1; j < tam; j++){
-            if(vetor[j] < vetor[aux]){
-                aux = j;
+            if(vetor[j] < vetor[aux]){ // Faz a seunda interação pra passar por todos os valores do vetor e comparar se o atual é menor ou maior
+                aux = j; // Se for menro faz a troca
             }
         }
-        if(aux != i){
+        if(aux != i){ // Caso o menor valor esteja em outra possição diferente de da vetor[i]
             int temp = vetor[i];
             vetor[i] = vetor[aux];
-            vetor[aux] = temp;
+            vetor[aux] = temp; // Fazemos a troca deles
         }
     }
     exibirVetor(vetor, tam);
@@ -52,7 +56,19 @@ void copiarVetorInicial(int *vetorInicial, int *vetorOrdenado, int tam){
         vetorOrdenado[i] = vetorInicial[i];
     }
 }
+// Funçaão para fazer um vetor reverso
+void vetorReverso(int *vetorOrdenado, int *vetorReverso, int tam){
+    int j = 0;
+    for(int i = tam -1;i >= 0;i--){
+        vetorReverso[j] = vetorOrdenado[i];
+        j++;
+    }
+}
 
+/*
+ * @André
+ * Ainda não entendi esses métodos
+*/
 void ordenarTrecho(int *vetor, int inicio, int fim){
     for(int i = inicio; i < fim; i++){
         int aux = i;
@@ -119,13 +135,13 @@ int main(){
 
     start = clock();
     printf("\n\t> Vetor Ordenado:\n");
-    ordenarVetor(vetorCopiado, tam);
+    selectionSort(vetorCopiado, tam);
     end = clock();
     double tempo2 = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     start = clock();
     printf("\n\t> Ordenar Novamente o Vetor:\n");
-    ordenarVetor(vetorCopiado, tam);
+    selectionSort(vetorCopiado, tam);
     end = clock();
     double tempo3 = ((double)(end - start)) / CLOCKS_PER_SEC;
 
